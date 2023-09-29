15bf42075c6771b260845e20985ed51e7e3459eb
guguuu <126880807+guguCEEP@users.noreply.github.com>
2023-09-29 09:03:15 -0300

    Add files via upload

---
+++ conversores.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// boolenos
+
+//conversão implícita
+const numero = 456;
+const numeroString = "456"
+
+// Number()
+//String()
+console.log(numero + numeroString)
+
+// conversão explícitas
---
+++ thuthy-falsy.js
@@ -0,0 +1,25 @@
+// boolean
+const usuarioLogado = true
+const contapaga = false
+
+// thuthy ou falsy
+
+// 0 =>  false
+// 1 => true
+
+//console.log(0== false)
+//console.log(""== false)
+//console.log(1== true)
+
+// underfind
+
+//null==> vazio ou nada
+
+let minhaVar;
+let varNull;
+
+let numero = 3;
+let texto =  "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
